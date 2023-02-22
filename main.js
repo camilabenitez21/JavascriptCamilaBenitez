0e7f6a5d7105f675cabe271be58ec61978924e1d
unknown <camilasoledadbenitez21@gmail.com>
2023-02-22 15:50:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+let correctas = [1,2,3,1,1]
+let opcion_elegida=[]
+let cantidad_correctas=0
+function respuesta(num_pregunta, seleccionada){
+    opcion_elegida[num_pregunta]= seleccionada.value;}
+
+    function corregir(){
+        cantidad_correctas= 0
+        for(i=0; i< correctas.length;i++){
+            id(correctas[i]==opcion_elegida[i]);{
+                cantidad_correctas++;
+            }
+        }
+        document.getElementById("resultado").innerHTML=cantidad_correctas
+    }
